84764d1f41ed822998a83108a59f7029c516430a
annadowling <anna.dowling@errigal.com>
2017-02-09 22:36:28 +0000

    Converted LoginActivity to use Volley rest

--- app/src/main/java/app/com/jobcatcherapp/activities/LoginActivity.java
+++ app/src/main/java/app/com/jobcatcherapp/activities/LoginActivity.java
@@ -15,34 +15,30 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import app.com.jobcatcherapp.R;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     EditText email, password, res_email, code, newpass;
     Button login, cont, cont_code, cancel, cancel1, register, forpass;
     String emailtxt, passwordtxt, email_res_txt, code_txt, npass_txt;
-    List<NameValuePair> params;
     SharedPreferences pref;
     Dialog reset;
-    ServerRequest serverRequest;
-    public static final String REQUEST_TAG = "LoginActivity";
-    private RequestQueue mQueue;
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        serverRequest = new ServerRequest();
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
@@ -52,146 +48,207 @@ public class LoginActivity extends AppCompatActivity {
 
         pref = getSharedPreferences("AppPref", MODE_PRIVATE);
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent regactivity = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(regactivity);
-                finish();
-            }
-        });
+        register.setOnClickListener(this);
+
+        login.setOnClickListener(this);
+
+        forpass.setOnClickListener(this);
+    }
 
 
-        login.setOnClickListener(new View.OnClickListener() {
+    public void register() {
+        Intent regactivity = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(regactivity);
+        finish();
+    }
 
+    public void login() {
+        emailtxt = email.getText().toString();
+        passwordtxt = password.getText().toString();
 
-            @Override
-            public void onClick(View view) {
-                emailtxt = email.getText().toString();
-                passwordtxt = password.getText().toString();
-                params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("email", emailtxt));
-                params.add(new BasicNameValuePair("password", passwordtxt));
-                ServerRequest serverRequest = new ServerRequest();
-                JSONObject json = serverRequest.getJSON("http://10.0.2.2:8080/login", params);
-                if (json != null) {
-                    try {
-                        String jsonstr = json.getString("response");
-                        if (json.getBoolean("res")) {
-                            String token = json.getString("token");
-                            String grav = json.getString("grav");
-                            SharedPreferences.Editor edit = pref.edit();
-                            //Storing Data using SharedPreferences
-                            edit.putString("token", token);
-                            edit.putString("grav", grav);
-                            edit.commit();
-                            Intent profactivity = new Intent(LoginActivity.this, ProfileActivity.class);
+        String url = "http://10.0.2.2:8080/login";
 
-                            startActivity(profactivity);
-                            finish();
-                        }
-
-                        Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        forpass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                reset = new Dialog(LoginActivity.this);
-                reset.setTitle("Reset Password");
-                reset.setContentView(R.layout.reset_password);
-                cont = (Button) reset.findViewById(R.id.resbtn);
-                cancel = (Button) reset.findViewById(R.id.cancelbtn);
-                cancel.setOnClickListener(new View.OnClickListener() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
                     @Override
-                    public void onClick(View view) {
-                        reset.dismiss();
-                    }
-                });
-                res_email = (EditText) reset.findViewById(R.id.email);
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject josnOBJ = new JSONObject(response);
+                            if (josnOBJ.getBoolean("res")) {
+                                String token = josnOBJ.getString("token");
+                                String grav = josnOBJ.getString("grav");
+                                SharedPreferences.Editor edit = pref.edit();
+                                //Storing Data using SharedPreferences
+                                edit.putString("token", token);
+                                edit.putString("grav", grav);
+                                edit.commit();
+                                Intent profactivity = new Intent(LoginActivity.this, ProfileActivity.class);
 
-                cont.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        email_res_txt = res_email.getText().toString();
-
-                        params = new ArrayList<NameValuePair>();
-                        params.add(new BasicNameValuePair("email", email_res_txt));
-
-                        JSONObject json = serverRequest.getJSON("http://10.0.2.2:8080/api/resetpass", params);
-
-                        if (json != null) {
-                            try {
-                                String jsonstr = json.getString("response");
-                                if (json.getBoolean("res")) {
-                                    Log.e("JSON", jsonstr);
-                                    Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
-                                    reset.setContentView(R.layout.reset_passcode);
-                                    cont_code = (Button) reset.findViewById(R.id.conbtn);
-                                    code = (EditText) reset.findViewById(R.id.code);
-                                    newpass = (EditText) reset.findViewById(R.id.npass);
-                                    cancel1 = (Button) reset.findViewById(R.id.cancel);
-                                    cancel1.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            reset.dismiss();
-                                        }
-                                    });
-                                    cont_code.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            code_txt = code.getText().toString();
-                                            npass_txt = newpass.getText().toString();
-                                            Log.e("Code", code_txt);
-                                            Log.e("New pass", npass_txt);
-                                            params = new ArrayList<NameValuePair>();
-                                            params.add(new BasicNameValuePair("email", email_res_txt));
-                                            params.add(new BasicNameValuePair("code", code_txt));
-                                            params.add(new BasicNameValuePair("newpass", npass_txt));
-
-                                            JSONObject json = serverRequest.getJSON("http://10.0.2.2:8080/api/resetpass/chg", params);
-
-                                            if (json != null) {
-                                                try {
-
-                                                    String jsonstr = json.getString("response");
-                                                    if (json.getBoolean("res")) {
-                                                        reset.dismiss();
-                                                        Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
-
-                                                    } else {
-                                                        Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
-
-                                                    }
-                                                } catch (JSONException e) {
-                                                    e.printStackTrace();
-                                                }
-                                            }
-
-                                        }
-                                    });
-                                } else {
-
-                                    Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_LONG).show();
-
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                                startActivity(profactivity);
+                                finish();
                             }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-
+                        Toast.makeText(getApplication(), response, Toast.LENGTH_LONG).show();
                     }
-                });
-
-
-                reset.show();
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(KEY_PASSWORD, passwordtxt);
+                params.put(KEY_EMAIL, emailtxt);
+                return params;
             }
-        });
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void forgotPassword() {
+        reset = new Dialog(LoginActivity.this);
+        reset.setTitle("Reset Password");
+        reset.setContentView(R.layout.reset_password);
+        cont = (Button) reset.findViewById(R.id.resbtn);
+        cancel = (Button) reset.findViewById(R.id.cancelbtn);
+        res_email = (EditText) reset.findViewById(R.id.email);
+
+        cancel.setOnClickListener(this);
+        cont.setOnClickListener(this);
+        reset.show();
+    }
+
+    public void cancel() {
+        reset.dismiss();
+    }
+
+    public void setListenersForButton(){
+        cancel1.setOnClickListener(this);
+        cont_code.setOnClickListener(this);
+    }
+
+    public void changePassword() {
+        email_res_txt = res_email.getText().toString();
+
+        String url = "http://10.0.2.2:8080/api/resetpass";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject josnOBJ = new JSONObject(response);
+                            if (josnOBJ.getBoolean("res")) {
+                                Log.e("JSON", josnOBJ.toString());
+                                Toast.makeText(getApplication(), response, Toast.LENGTH_LONG).show();
+                                reset.setContentView(R.layout.reset_passcode);
+                                cont_code = (Button) reset.findViewById(R.id.conbtn);
+                                code = (EditText) reset.findViewById(R.id.code);
+                                newpass = (EditText) reset.findViewById(R.id.npass);
+                                cancel1 = (Button) reset.findViewById(R.id.cancel);
+
+                                setListenersForButton();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(getApplication(), response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email_res_txt);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+
+    public void setCode() {
+        code_txt = code.getText().toString();
+        npass_txt = newpass.getText().toString();
+        Log.e("Code", code_txt);
+        Log.e("New pass", npass_txt);
+
+        String url = "http://10.0.2.2:8080/api/resetpass/chg";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject josnOBJ = new JSONObject(response);
+                            if (josnOBJ.getBoolean("res")) {
+                                reset.dismiss();
+                                Toast.makeText(getApplication(), response, Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplication(), response, Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email_res_txt);
+                params.put("code", code_txt);
+                params.put("newpass", npass_txt);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == register) {
+            register();
+        } else if (v == login) {
+            login();
+
+        } else if (v == forpass) {
+            forgotPassword();
+        } else if (v == cancel) {
+            cancel();
+        } else if (v == cont) {
+            changePassword();
+        } else if (v == cancel1) {
+            cancel();
+        } else if (v == cont_code) {
+            setCode();
+        }
     }
 }
+
--- app/src/main/java/app/com/jobcatcherapp/activities/ProfileActivity.java
+++ app/src/main/java/app/com/jobcatcherapp/activities/ProfileActivity.java
@@ -21,13 +21,17 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import app.com.jobcatcherapp.R;
 
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     SharedPreferences pref;
@@ -74,72 +78,76 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         web.loadUrl(grav);
     }
 
-    private void changePassword(){
-                dlg = new Dialog(ProfileActivity.this);
-                dlg.setContentView(R.layout.chgpassword_fragment);
-                dlg.setTitle("Change Password");
-                chgpassfr = (Button) dlg.findViewById(R.id.chgbtn);
+    private void changePassword() {
+        dlg = new Dialog(ProfileActivity.this);
+        dlg.setContentView(R.layout.chgpassword_fragment);
+        dlg.setTitle("Change Password");
+        chgpassfr = (Button) dlg.findViewById(R.id.chgbtn);
 
-                chgpassfr.setOnClickListener(this);
+        chgpassfr.setOnClickListener(this);
 
-                cancel = (Button) dlg.findViewById(R.id.cancelbtn);
-                cancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dlg.dismiss();
-                    }
-                });
-                dlg.show();
+        cancel = (Button) dlg.findViewById(R.id.cancelbtn);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dlg.dismiss();
             }
-
-    private void handleChangePasswordFragment(){
-                oldpass = (EditText) dlg.findViewById(R.id.oldpass);
-                newpass = (EditText) dlg.findViewById(R.id.newpass);
-                oldpasstxt = oldpass.getText().toString();
-                newpasstxt = newpass.getText().toString();
-
-                String url = "http://10.0.2.2:8080/api/chgpass";
-
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                if (response.contains("\"res\":true")) {
-                                    dlg.dismiss();
-                                    Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(ProfileActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put(KEY_OLDPASSWORD, oldpasstxt);
-                        params.put(KEY_NEWPASSWORD, newpasstxt);
-                        params.put(KEY_ID, token);
-                        return params;
-                    }
-
-                };
-
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                requestQueue.add(stringRequest);
+        });
+        dlg.show();
     }
 
+    private void handleChangePasswordFragment() {
+        oldpass = (EditText) dlg.findViewById(R.id.oldpass);
+        newpass = (EditText) dlg.findViewById(R.id.newpass);
+        oldpasstxt = oldpass.getText().toString();
+        newpasstxt = newpass.getText().toString();
+
+        String url = "http://10.0.2.2:8080/api/chgpass";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject josnOBJ = new JSONObject(response);
+                            if (josnOBJ.getBoolean("res")) {
+                                dlg.dismiss();
+                                Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(ProfileActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(KEY_OLDPASSWORD, oldpasstxt);
+                params.put(KEY_NEWPASSWORD, newpasstxt);
+                params.put(KEY_ID, token);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 
 
     @Override
     public void onClick(View v) {
-        if(v == chgpass){
+        if (v == chgpass) {
             changePassword();
-        }else if(v == chgpassfr){
+        } else if (v == chgpassfr) {
             handleChangePasswordFragment();
         }
     }
