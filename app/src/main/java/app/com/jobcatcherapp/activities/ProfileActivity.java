b45f11866075fe88c19fcd32b0921a2bd97ab7ad
annadowling <anna.dowling@errigal.com>
2017-02-09 18:09:54 +0000

    Converted ProfileActivity to use volley rest handling

--- app/src/main/java/app/com/jobcatcherapp/activities/ProfileActivity.java
+++ app/src/main/java/app/com/jobcatcherapp/activities/ProfileActivity.java
@@ -15,18 +15,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
 
 import app.com.jobcatcherapp.R;
 
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     SharedPreferences pref;
@@ -35,7 +36,9 @@ public class ProfileActivity extends AppCompatActivity {
     Button chgpass, chgpassfr, cancel, logout;
     Dialog dlg;
     EditText oldpass, newpass;
-    List<NameValuePair> params;
+    public static final String KEY_OLDPASSWORD = "oldpass";
+    public static final String KEY_NEWPASSWORD = "newpass";
+    public static final String KEY_ID = "id";
 
 
     @Override
@@ -45,13 +48,16 @@ public class ProfileActivity extends AppCompatActivity {
         web = (WebView) findViewById(R.id.webView);
         chgpass = (Button) findViewById(R.id.chgbtn);
         logout = (Button) findViewById(R.id.logout);
+
+        chgpass.setOnClickListener(this);
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences.Editor edit = pref.edit();
                 //Storing Data using SharedPreferences
                 edit.putString("token", "");
-                edit.commit(); //TODO look at using apply() instead
+                edit.commit();
                 Intent loginactivity = new Intent(ProfileActivity.this, LoginActivity.class);
 
                 startActivity(loginactivity);
@@ -66,46 +72,16 @@ public class ProfileActivity extends AppCompatActivity {
         web.getSettings().setUseWideViewPort(true);
         web.getSettings().setLoadWithOverviewMode(true);
         web.loadUrl(grav);
+    }
 
-        chgpass.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private void changePassword(){
                 dlg = new Dialog(ProfileActivity.this);
                 dlg.setContentView(R.layout.chgpassword_fragment);
                 dlg.setTitle("Change Password");
                 chgpassfr = (Button) dlg.findViewById(R.id.chgbtn);
 
-                chgpassfr.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        oldpass = (EditText) dlg.findViewById(R.id.oldpass);
-                        newpass = (EditText) dlg.findViewById(R.id.newpass);
-                        oldpasstxt = oldpass.getText().toString();
-                        newpasstxt = newpass.getText().toString();
-                        params = new ArrayList<NameValuePair>();
-                        params.add(new BasicNameValuePair("oldpass", oldpasstxt));
-                        params.add(new BasicNameValuePair("newpass", newpasstxt));
-                        params.add(new BasicNameValuePair("id", token));
-                        ServerRequest sr = new ServerRequest();
-                        JSONObject json = sr.getJSON("http://10.0.2.2:8080/api/chgpass", params);
-                        if (json != null) {
-                            try {
-                                String jsonstr = json.getString("response");
-                                if (json.getBoolean("res")) {
+                chgpassfr.setOnClickListener(this);
 
-                                    dlg.dismiss();
-                                    Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(getApplication(), jsonstr, Toast.LENGTH_SHORT).show();
-
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                    }
-                });
                 cancel = (Button) dlg.findViewById(R.id.cancelbtn);
                 cancel.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -115,10 +91,56 @@ public class ProfileActivity extends AppCompatActivity {
                 });
                 dlg.show();
             }
-        });
 
+    private void handleChangePasswordFragment(){
+                oldpass = (EditText) dlg.findViewById(R.id.oldpass);
+                newpass = (EditText) dlg.findViewById(R.id.newpass);
+                oldpasstxt = oldpass.getText().toString();
+                newpasstxt = newpass.getText().toString();
 
+                String url = "http://10.0.2.2:8080/api/chgpass";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (response.contains("\"res\":true")) {
+                                    dlg.dismiss();
+                                    Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplication(), response, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(ProfileActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put(KEY_OLDPASSWORD, oldpasstxt);
+                        params.put(KEY_NEWPASSWORD, newpasstxt);
+                        params.put(KEY_ID, token);
+                        return params;
+                    }
+
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
     }
 
 
+
+    @Override
+    public void onClick(View v) {
+        if(v == chgpass){
+            changePassword();
+        }else if(v == chgpassfr){
+            handleChangePasswordFragment();
+        }
+    }
 }
