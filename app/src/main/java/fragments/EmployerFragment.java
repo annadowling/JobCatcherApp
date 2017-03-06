cd920f3b9728cb8565d67b363358116b909232f3
annadowling <anna.dowling@errigal.com>
2017-03-06 20:15:40 +0000

    Added register employer logic

--- app/src/main/java/fragments/EmployerFragment.java
+++ app/src/main/java/fragments/EmployerFragment.java
@@ -7,8 +7,14 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import app.com.jobcatcherapp.R;
+import requests.VolleyRequest;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,8 +24,18 @@ import app.com.jobcatcherapp.R;
  * Use the {@link EmployerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EmployerFragment extends Fragment {
+public class EmployerFragment extends Fragment implements View.OnClickListener{
     private OnFragmentInteractionListener mListener;
+
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_COMPANYNAME = "companyName";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_LATITUDE = "latitude";
+    public static final String KEY_LONGITUDE = "longitude";
+    EditText email, companyName, password, address, latitude, longitude ;
+    Button login, register;
+    String emailtxt, passwordtxt, addresstxt, companyNametxt, latitudetxt, longitudetxt;
 
     public EmployerFragment() {
         // Required empty public constructor
@@ -39,7 +55,19 @@ public class EmployerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employer, container, false);
+        View view = inflater.inflate(R.layout.fragment_employer, container, false);
+        email = (EditText) view.findViewById(R.id.employer_email);
+        companyName = (EditText) view.findViewById(R.id.company_name);
+        address = (EditText) view.findViewById(R.id.address);
+        latitude = (EditText) view.findViewById(R.id.latitude);
+        longitude = (EditText) view.findViewById(R.id.longitude);
+        password = (EditText) view.findViewById(R.id.employerPassword);
+        register = (Button) view.findViewById(R.id.register_employer);
+        //login = (Button) view.findViewById(R.id.);
+
+        register.setOnClickListener(this);
+        //login.setOnClickListener(this);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -79,5 +107,34 @@ public class EmployerFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void registerEmployer() {
+        //email, companyName, address, latitude, longitude, password
+        emailtxt = email.getText().toString();
+        companyNametxt = companyName.getText().toString();
+        addresstxt = address.getText().toString();
+        latitudetxt = latitude.getText().toString();
+        longitudetxt = longitude.getText().toString();
+        passwordtxt = password.getText().toString();
+        String url = "http://10.0.2.2:8080/registerEmployer";
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put(KEY_PASSWORD, passwordtxt);
+        params.put(KEY_COMPANYNAME, companyNametxt);
+        params.put(KEY_ADDRESS, addresstxt);
+        params.put(KEY_LATITUDE, latitudetxt);
+        params.put(KEY_LONGITUDE, longitudetxt);
+        params.put(KEY_EMAIL, emailtxt);
+
+        VolleyRequest request = new VolleyRequest();
+        request.makeVolleyRequest(getActivity().getApplicationContext(), params, url);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == register) {
+            registerEmployer();
+        }
     }
 }
